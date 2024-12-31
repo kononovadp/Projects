8f2d875054b3e3d43ba4b5409186f605b2cb7b8a
kononovadp <56555804+kononovadp@users.noreply.github.com>
2024-12-31 04:31:12 +0200

    Add files via upload

---
+++ C++/complier/CompileDelphiProgram/program.pas
@@ -0,0 +1,252 @@
+program JJcx3tAmp; //some comment
+Uses Math;
+var
+	bssB, fitdA6QA, IntZero: integer;
+	snd, PTwxeewbaf: longint;
+	ahwqXtKA, gLb, FloatZero: float;
+	tt4R8qheH, j6dBj, hd4, ecek, result: extended;
+	WusClt, f8h5eMlro: boolean;
+	power1, power2, power3: integer;
+	o, c, k, i, t, p, u, m, f, e, j, b, d, z, y, r, h, v, l, x, n, w, a, s: integer;
+begin
+	bssB := 3;
+	fitdA6QA := 73;
+	IntZero := 0;
+	snd := 734991;
+	PTwxeewbaf := 5;
+	ahwqXtKA := 25471.703;
+	gLb := 527.466;
+	FloatZero := 0.0;
+	tt4R8qheH := 535.771;
+	j6dBj := 457.16;
+	hd4 := 0.547;
+	ecek := 0.445;
+	result := 0.0;
+	WusClt := false;
+	f8h5eMlro := false;
+	power1 := 19;
+	power2 := 3;
+	power3 := 0;
+
+	//----------SOME COMMENTS 1----------
+	if snd > 711587 then
+	begin
+		if gLb < gLb then
+		begin
+			IntZero := power1;
+			writeln('power2: ',power2,#13#10,'PTwxeewbaf: ',PTwxeewbaf,#13#10);
+		end;
+		for o := power1 to power1 do
+		begin
+			writeln('WusClt: ',WusClt,#13#10,'gLb: ',gLb:0:5,#13#10,'WusClt: ',WusClt,#13#10,'ahwqXtKA: ',ahwqXtKA:0:5,#13#10);
+			result := ((2 * FloatZero) + (ahwqXtKA ** power2)) / (gLb - 442) ** 4;
+			bssB := power1;
+		end;
+		for o := 267 to bssB do
+		begin
+			j6dBj := gLb;
+			result := ((((IntZero - 441.1347) * result + gLb) / ((137.41 + 85.8708) ** power3)) * (gLb - PTwxeewbaf / 910 + PTwxeewbaf) ** 2);
+			snd := 688902;
+			f8h5eMlro := false;
+		end;
+	end;
+
+	//----------SOME COMMENTS 2----------
+	for o := power2 to 6060 do
+	begin
+		for c := IntZero to power2 do
+		begin
+			if snd = snd then
+			begin
+				gLb := power1;
+				snd := 49;
+			end;
+			for k := IntZero to 7619 do
+			begin
+				result := (((6 - snd) * 8773.01 / FloatZero) ** power1) - ((snd / j6dBj) + 1 * 0.4) * (ahwqXtKA - bssB) / 0.445 + IntZero;
+				ecek := bssB;
+				writeln('FloatZero: ',FloatZero:0:5,#13#10,'hd4: ',hd4:0:5,#13#10,'WusClt: ',WusClt,#13#10);
+			end;
+			if snd <> PTwxeewbaf then
+			begin
+				result := ((snd ** 7) + 61637.577 - 6521.1 ** power2);
+				ahwqXtKA := gLb;
+			end;
+			for k := bssB to power3 do
+			begin
+				writeln('result: ',result:0:5,#13#10,'WusClt: ',WusClt,#13#10,'FloatZero: ',FloatZero:0:5,#13#10,'power3: ',power3,#13#10);
+				result := ((snd * 588.4871) / 11.5716 / result);
+				writeln('gLb: ',gLb:0:5,#13#10,'result: ',result:0:5,#13#10);
+			end;
+		end;
+		if power1 > bssB then
+		begin
+			for c := 856 to power2 do
+				bssB := 6;
+			for c := 49 to power3 do
+			begin
+				writeln('ahwqXtKA: ',ahwqXtKA:0:5,#13#10,'power2: ',power2,#13#10,'result: ',result:0:5,#13#10);
+				snd := 52111;
+				writeln('PTwxeewbaf: ',PTwxeewbaf,#13#10,'power2: ',power2,#13#10,'PTwxeewbaf: ',PTwxeewbaf,#13#10,'power1: ',power1,#13#10);
+			end;
+		end;
+	end;
+
+	//----------SOME COMMENTS 3----------
+	for o := 313 to 40 do
+	begin
+		for c := 614 to fitdA6QA do
+		begin
+			tt4R8qheH := IntZero;
+			writeln('power1: ',power1,#13#10,'ahwqXtKA: ',ahwqXtKA:0:5,#13#10);
+			result := (j6dBj / 262.4231 - (74773.405 + 7882)) ** 8 / hd4 + 73552.8 ** power1 * (hd4 - snd + 1074 / PTwxeewbaf);
+		end;
+		for c := 76 to 9 do
+		begin
+			writeln('ahwqXtKA: ',ahwqXtKA:0:5,#13#10,'result: ',result:0:5,#13#10);
+			result := ((ahwqXtKA - 5048) ** 9) * (ecek - 403.414 * (0.1216 ** power3));
+			result := ((5 / gLb) + snd + 62317.846 * (3 / IntZero) - (j6dBj ** power2) ** power1);
+		end;
+	end;
+
+	//----------SOME COMMENTS 4----------
+	if f8h5eMlro < true then
+	begin
+		for o := bssB to power1 do
+		begin
+			for c := 46 to bssB do
+			begin
+				for k := power1 to power3 do
+					result := ((((0.7673 + 8715.1) / (PTwxeewbaf - 65.28)) * (6820 + 6 - (PTwxeewbaf * 83352.4153))) / (IntZero ** 4) + 887.5 * j6dBj ** 7);
+				for k := power3 to bssB do
+				begin
+					PTwxeewbaf := 4120;
+					result := IntZero / 8 - (2003.7708 * snd);
+					gLb := FloatZero;
+					result := ((snd ** 6) + (ahwqXtKA / 7445)) - PTwxeewbaf / ahwqXtKA ** power3 - ((22758.4 + 85004.7553) * (2602.328 / 5368.11)) - IntZero * 3329 + (42.538 ** power1);
+				end;
+				if ahwqXtKA = ahwqXtKA then
+					result := ((snd / bssB) * 7703.051 + 0.71);
+				for k := power1 to power2 do
+				begin
+					IntZero := power3;
+					result := ((0.135 ** power2) - 405 / 84);
+					result := 43.41 - 707 ** 4;
+				end;
+			end;
+			if power1 <> power1 then
+			begin
+				for c := 7279 to IntZero do
+				begin
+					writeln('WusClt: ',WusClt,#13#10,'power1: ',power1,#13#10,'IntZero: ',IntZero,#13#10);
+					WusClt := f8h5eMlro;
+					WusClt := false;
+					result := (ahwqXtKA + snd * snd - 22126.158);
+				end;
+				for c := 6612 to bssB do
+				begin
+					ahwqXtKA := bssB;
+					result := ((3 + 3 ** 2) * (3078 / fitdA6QA) / (hd4 - 63) + (840 * 686.878 ** power2 * 4 + IntZero / PTwxeewbaf - tt4R8qheH));
+					writeln('power2: ',power2,#13#10,'f8h5eMlro: ',f8h5eMlro,#13#10);
+					WusClt := true;
+				end;
+				for c := fitdA6QA to power2 do
+				begin
+					f8h5eMlro := false;
+					tt4R8qheH := power1;
+				end;
+			end;
+			for c := IntZero to power1 do
+			begin
+				for k := 64 to 4 do
+				begin
+					snd := 13143;
+					result := (((10.12 ** power2) / (1 + 57) * ((23.871 ** power3) - (4800 - bssB))) + (((763.8 * snd) ** power3) / (0.88 * ahwqXtKA) + fitdA6QA ** power3));
+				end;
+				for k := fitdA6QA to power1 do
+				begin
+					writeln('power1: ',power1,#13#10,'WusClt: ',WusClt,#13#10,'result: ',result:0:5,#13#10);
+					j6dBj := PTwxeewbaf;
+					result := (hd4 / 254) - 84205.2 - 0.7714 / (9 ** power1 * (1405.448 + 0.58));
+					ahwqXtKA := ahwqXtKA;
+				end;
+				if f8h5eMlro > WusClt then
+					writeln('f8h5eMlro: ',f8h5eMlro,#13#10);
+			end;
+			for c := IntZero to fitdA6QA do
+			begin
+				for k := 8685 to 60 do
+					writeln('f8h5eMlro: ',f8h5eMlro,#13#10,'hd4: ',hd4:0:5,#13#10,'result: ',result:0:5,#13#10);
+				if hd4 < 23443.06648 then
+				begin
+					result := (gLb * 2002.4 + 732 ** 3) / (20452.3 - 58.5585 - (756 + ahwqXtKA));
+					result := (((8170.4 ** power3) / (87.8375 * 793)) / (PTwxeewbaf + 70106.752 ** power1));
+					writeln('WusClt: ',WusClt,#13#10,'ahwqXtKA: ',ahwqXtKA:0:5,#13#10);
+					writeln('FloatZero: ',FloatZero:0:5,#13#10,'PTwxeewbaf: ',PTwxeewbaf,#13#10);
+				end;
+			end;
+		end;
+		for o := fitdA6QA to 347 do
+		begin
+			if power2 = power2 then
+				for c := power2 to 661 do
+				begin
+					result := (((fitdA6QA - 8184.4) * (743.513 + 47864.6)) * (fitdA6QA - PTwxeewbaf / FloatZero ** power3));
+					result := ((IntZero ** 6 + 1 * 302 - ((55854.354 / snd) / 5996 ** 9)) - (53.24 * ecek) + PTwxeewbaf * FloatZero + FloatZero / PTwxeewbaf ** 5);
+				end;
+			if ahwqXtKA <> gLb then
+				for c := IntZero to fitdA6QA do
+				begin
+					result := ecek;
+					result := ((ahwqXtKA - result + (FloatZero - ahwqXtKA) ** 4) * ((gLb / tt4R8qheH * bssB + IntZero) - (134 / 383.666 ** 6)));
+				end;
+			for c := power1 to bssB do
+			begin
+				for k := power2 to fitdA6QA do
+					result := ((j6dBj / bssB) - fitdA6QA ** 2 * (763.65 + 18350.3401) * FloatZero + 7162.8) ** 2;
+				for k := 4 to power2 do
+				begin
+					writeln('WusClt: ',WusClt,#13#10,'IntZero: ',IntZero,#13#10,'ahwqXtKA: ',ahwqXtKA:0:5,#13#10);
+					writeln('gLb: ',gLb:0:5,#13#10,'IntZero: ',IntZero,#13#10,'bssB: ',bssB,#13#10);
+					writeln('IntZero: ',IntZero,#13#10,'tt4R8qheH: ',tt4R8qheH:0:5,#13#10,'WusClt: ',WusClt,#13#10,'IntZero: ',IntZero,#13#10);
+					writeln('snd: ',snd,#13#10,'WusClt: ',WusClt,#13#10);
+				end;
+				for k := 381 to power1 do
+				begin
+					gLb := power2;
+					snd := snd;
+					result := (snd / 7) - (9071 / snd);
+					writeln('PTwxeewbaf: ',PTwxeewbaf,#13#10,'WusClt: ',WusClt,#13#10);
+				end;
+			end;
+		end;
+		for o := power2 to 1952 do
+			if FloatZero > gLb then
+			begin
+				if ecek < power3 then
+				begin
+					snd := PTwxeewbaf;
+					result := (FloatZero - 23.243 ** 3) * ((4381.1075 + 876) / (tt4R8qheH ** power1)) - 1220.351 * ahwqXtKA + 3270.045 + snd ** 8;
+					writeln('gLb: ',gLb:0:5,#13#10,'f8h5eMlro: ',f8h5eMlro,#13#10,'gLb: ',gLb:0:5,#13#10,'j6dBj: ',j6dBj:0:5,#13#10);
+				end;
+				for c := power3 to 3206 do
+				begin
+					writeln('ahwqXtKA: ',ahwqXtKA:0:5,#13#10,'IntZero: ',IntZero,#13#10,'snd: ',snd,#13#10,'power1: ',power1,#13#10);
+					f8h5eMlro := false;
+					gLb := IntZero;
+					writeln('gLb: ',gLb:0:5,#13#10,'PTwxeewbaf: ',PTwxeewbaf,#13#10,'snd: ',snd,#13#10,'snd: ',snd,#13#10);
+				end;
+			end;
+		for o := 9077 to 22 do
+			for c := power2 to power1 do
+			begin
+				for k := fitdA6QA to 726 do
+				begin
+					writeln('power1: ',power1,#13#10);
+					fitdA6QA := power1;
+				end;
+				for k := power1 to power3 do
+					fitdA6QA := 14;
+			end;
+	end;
+end.

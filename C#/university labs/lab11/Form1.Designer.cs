9b5dc76a845469945827645ddf29434fbdb98a4c
kononovadp <56555804+kononovadp@users.noreply.github.com>
2026-03-18 05:26:03 +0200

    Add files via upload

---
+++ C#/university labs/lab11/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace lab11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.graph = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.graph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // graph
+            // 
+            this.graph.Location = new System.Drawing.Point(21, 21);
+            this.graph.Margin = new System.Windows.Forms.Padding(12);
+            this.graph.Name = "graph";
+            this.graph.Size = new System.Drawing.Size(950, 628);
+            this.graph.TabIndex = 0;
+            this.graph.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(992, 670);
+            this.Controls.Add(this.graph);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Лабораторна робота №11";
+            ((System.ComponentModel.ISupportInitialize)(this.graph)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox graph;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ C#/university labs/lab11/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.Threading;
+namespace lab11//https://www.cyberforum.ru/python-beginners/thread2152031.html
+{
+    public partial class Form1 : Form
+    {
+        float x,y,StepSize,XStep,YStep,RandomDistance;
+        int RectangleX,RectangleY;
+        Point[] triangle;
+        Random rnd;
+        Image MovingImage;
+        Pen RedPen;
+        PointF corner;
+        Graphics g;
+        byte xplus,yplus;
+        public Form1()
+        {            
+            InitializeComponent();
+            rnd=new Random();
+            x=0;            
+            y=graph.Height-128;
+            xplus=1;
+            yplus=1;
+            StepSize=graph.Height/20;            
+            RectangleX=graph.Width/2-50;
+            RectangleY=graph.Height/2-50;
+            triangle=new Point[3];
+            triangle[0]=new Point(RectangleX+200,RectangleY+100);
+            triangle[1]=new Point(RectangleX+250,RectangleY);
+            triangle[2]=new Point(RectangleX+300,RectangleY+100);
+            corner=new PointF(x, y);
+            MovingImage = Image.FromFile("image.jpg");
+            RedPen = new Pen(Color.Red, 3);
+            graph.Image = new Bitmap(graph.Width, graph.Height);
+            g = Graphics.FromImage(graph.Image);
+            timer1.Interval=40;
+            timer1.Start();            
+        }
+        private byte CheckCircle()
+        {
+            //https://yandex.ru/q/question/kak_pravilno_opredelit_prinadlezhnost_k_e51074a4/
+            double SquaredRadius=100*100,xcenter=graph.Width/2,ycenter=graph.Height/2,limit;
+            double LeftCorner=graph.Width/2-100,RightCorner=graph.Width/2+100,UpperCorner=graph.Height/2-100,LowerCorner=graph.Height/2+100;
+            double NextX=x+XStep,NextY=y+YStep;
+            int i,StepsCount;
+            if(xplus==1)
+                NextX=x+XStep;
+            else
+                NextX=x-XStep;
+            if(yplus==1)
+                NextY=y+YStep;
+            else
+                NextY=y-YStep;
+            if(NextX+128<LeftCorner || NextX>RightCorner || NextY+128<UpperCorner || NextY>LowerCorner)
+                return 0;
+            if(NextX<LeftCorner)
+                NextX=LeftCorner;
+            if(NextY<UpperCorner)
+                NextY+=128;
+            for(i=0,StepsCount=Convert.ToInt32(RightCorner)-Convert.ToInt32(NextX); i<StepsCount; i++,NextX++)
+                if(((NextX-xcenter)*(NextX-xcenter))+((NextY-ycenter)*(NextY-ycenter))<=SquaredRadius)
+                    return 1;
+            if(NextY<UpperCorner)
+                NextY=UpperCorner;
+            for(i=0,StepsCount=Convert.ToInt32(LowerCorner)-Convert.ToInt32(NextY); i<StepsCount; i++,NextY++)
+                if(((NextX-xcenter)*(NextX-xcenter))+((NextY-ycenter)*(NextY-ycenter))<=SquaredRadius)
+                    return 1;
+            return 0;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            graph.Image = new Bitmap(graph.Width, graph.Height);
+            g = Graphics.FromImage(graph.Image);
+            g.DrawRectangle(RedPen,0,0,graph.Width-1, graph.Height-1);
+            g.DrawEllipse(RedPen,graph.Width/2-100,graph.Height/2-100,200,200);
+            //g.DrawRectangle(RedPen, RectangleX, RectangleY, 100, 100);
+            //g.DrawPolygon(RedPen,triangle);
+            corner.X=x;
+            corner.Y=y;
+            g.DrawImage(MovingImage,corner);
+            if(x<=0 || x>=graph.Width-128 || y<=0 || y>=graph.Height-128 || CheckCircle()==1)
+            {
+                if(xplus==0)
+                {
+                    XStep=(rnd.Next(Convert.ToInt32(x+1),graph.Width)-x)/StepSize;
+                    xplus=1;
+                }
+                else
+                    if(xplus==1)
+                    {
+                        XStep=(x-rnd.Next(0,Convert.ToInt32(x)))/StepSize;
+                        xplus=0;
+                    }
+                if(yplus==0)
+                {
+                    YStep=(graph.Height-128-y)/StepSize;
+                    yplus=1;
+                }
+                else
+                    if(yplus==1)
+                    {
+                        YStep=y/StepSize;
+                        yplus=0;
+                    }
+            }
+            if(xplus==1)
+                x+=XStep;
+            else
+                if(xplus==0)
+                    x-=XStep;
+            if(yplus==1)
+                y+=YStep;
+            else
+                if(yplus==0)
+                    y-=YStep;
+        }
+    }
+}
